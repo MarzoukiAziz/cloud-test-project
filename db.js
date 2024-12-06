69464d496d6a9003741944b0cc371614dc167d94
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-12-06 02:17:38 +0100

    ya star

--- db.js
+++ db.js
@@ -6,6 +6,9 @@ const client = new Client({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 client

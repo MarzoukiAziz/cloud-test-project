3e435226342a311985fe8937bdfb1c0c0177b3eb
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-12-06 01:32:39 +0100

    ya rab ya rab

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 //Configuring Dotenv to use environment variables from .env file
 const dotenv = require('dotenv');
-const environment = process.env.NODE_ENV || 'local';
+const environment = process.env.NODE_ENV || 'dev';
 dotenv.config({ path: `.env.${environment}` });
 console.log('env : ' + environment);
 
